b58d85e07173062e80dbd26c5ce375ce85999c01
OpltFrantisek <opltfrantisek@gmail.com>
2017-10-05 20:44:47 +0200

    prijmame sms

--- smskovac/src/main.cpp
+++ smskovac/src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-void setup() {
-    // put your setup code here, to run once:
+#include <DTE.h>
+#include <GSM.h>
+#include <SMS.h>
+#include <URC.h>
+
+#define PINPOWER 9
+#define DEBUG false
+
+SoftwareSerial SSerial(10, 11);
+DTE dte(SSerial, PINPOWER, DEBUG);
+SMS sms(dte);
+GSM gsm(dte);
+
+void setup(void) {
+  Serial.begin(9600);
+  SSerial.begin(19200);
+  Serial.println("Zapinam smskovac...");
+  dte.powerReset();
+  Serial.println("cekam na sit...");
+  while (gsm.getNetworkRegistration().status != 1)
+    ;
+  Serial.print("pripojuji ");
+  Serial.println(gsm.getOperator().oper);
+
+ // Serial.print("Try send SMS To ");
+ // Serial.println(gsm.getSubscriberNumber().number);
+  //Serial.println();
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop(void) {
+  if (!dte.AT())
+    dte.powerReset();
+
+  if (Urc.newMessageIndication.updated) {
+    struct Message m = sms.readSMS(Urc.newMessageIndication.index);
+    Serial.println(m.address);
+    Serial.println(m.data);
+    Urc.newMessageIndication.updated = false;
+  }
 }
